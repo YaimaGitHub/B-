9703d0f32145cb6d67ca3dcf3f9794061f04804e
YaimaGitHub <tvalacarta33@gmail.com>
2025-07-01 11:26:33 -0400

    full
    full

--- src/frontend/components/Hero/Hero.jsx
+++ src/frontend/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import styles from './Hero.module.css';
 
-import jethalalBanner from '../../assets/jethalalBanner.jpg';
+import jethalalBanner from '../../assets/jethalalBanner.png';
 import { Link } from 'react-router-dom';
 import { useIsMobile } from '../../hooks';
 
