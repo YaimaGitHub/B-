9f65f80cfa1631c821d3f40478eee5243f2098fd
YaimaGitHub <tvalacarta33@gmail.com>
2025-07-04 16:15:31 -0400

    cupones deshabilitados
    cupones deshabilitados

--- src/frontend/constants/constants.jsx
+++ src/frontend/constants/constants.jsx
@@ -257,7 +257,7 @@ export const DEFAULT_STORE_CONFIG = {
   "storeName": "Yero Shop!",
   "whatsappNumber": "+53 54690878",
   "storeAddress": "Santiago de Cuba, Cuba",
-  "lastModified": "2025-07-04T18:00:45.832Z",
+  "lastModified": "2025-07-04T20:13:21.822Z",
   "version": "1.0.0"
 };
 
@@ -317,21 +317,21 @@ export const CURRENCIES = {
     name: 'Dólar Estadounidense',
     symbol: '$',
     flag: '🇺🇸',
-    rate: 384,
+    rate: 320,
   },
   EUR: {
     code: 'EUR',
     name: 'Euro',
     symbol: '€',
     flag: '🇪🇺',
-    rate: 425,
+    rate: 340,
   },
   MLC: {
     code: 'MLC',
     name: 'Moneda Libremente Convertible',
     symbol: 'MLC',
     flag: '🏦',
-    rate: 250,
+    rate: 270,
   },
 };
 
